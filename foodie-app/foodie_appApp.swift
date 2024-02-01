2b8322779500c4242dbc9bb1fce716dfff82e175
Heri Susanto <herisusanto.app@gmail.com>
2024-02-01 19:24:52 +0700

    Initial Commit

---
+++ foodie-app/foodie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodie_appApp.swift
+//  foodie-app
+//
+//  Created by loratech on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct foodie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
