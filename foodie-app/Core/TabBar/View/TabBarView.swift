ca5b0e324308f03440a1bf77bac4b16e22880760
Heri Susanto <herisusanto.app@gmail.com>
2024-02-02 16:41:31 +0700

    Add tab bar view

---
+++ foodie-app/Core/TabBar/View/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  foodie-app
+//
+//  Created by loratech on 02/02/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
