2e1a5d8e595ed4386965acbc1b594483f7172c9f
Heri Susanto <herisusanto.app@gmail.com>
2024-02-05 17:24:23 +0700

    Add Menu Item View Add Food model

---
+++ foodie-app/Core/Components/MenuItemView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemView.swift
+//  foodie-app
+//
+//  Created by loratech on 05/02/24.
+//
+
+import SwiftUI
+
+struct MenuItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemView()
+}
---
+++ foodie-app/Model/Food.swift
@@ -0,0 +1,8 @@
+//
+//  Food.swift
+//  foodie-app
+//
+//  Created by loratech on 05/02/24.
+//
+
+import Foundation
